998b95bb6182db4bc9a2c7459f9aec68ed5206ab
kimmy <kenpusney@gmail.com>
2020-12-08 14:49:11 +0800

    MSVC compilable

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -287,11 +287,30 @@ namespace NG::Parsing {
 
     static int hex2dec(char c) {
         switch (c) {
-            case '0' ... '9':
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
                 return c - '0';
-            case 'a' ... 'f':
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
                 return (c - 'a') + 10;
-            case 'A' ... 'F':
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            case 'F':
                 return (c - 'A') + 10;
             default:
                 throw LexException("Unknown hex digit");
