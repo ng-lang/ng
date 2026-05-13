d54397c09e8cdc3d8e1e73cc0785dd61c08e5bfe
Kimmy <kenpusney@gmail.com>
2026-05-13 20:47:41 +0800

    test(integration): restore loop stack coverage

--- test/integration_test.cpp
+++ test/integration_test.cpp
@@ -36,7 +36,7 @@ static inline void runIntegrationTest(const std::string &filename)
 
 TEST_CASE("should run with max loop stack", "[IntegrationLoop]")
 {
-  SKIP("example/12.loop_max_stack.ng still needs a real stack-growth fix before this can be asserted.");
+  runIntegrationTest("example/12.loop_max_stack.ng");
 }
 
 TEST_CASE("should run numbered examples", "[Integration]")
